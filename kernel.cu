129843b88b51638b459362d8f8a63585c57baa02
Adrian Madajewski <madajewski.adrian@gmail.com>
2022-06-10 17:26:54 +0200

    fixes bytes size

--- kernel.cu
+++ kernel.cu
@@ -7,11 +7,6 @@
 #include <ctime>
 #include <iostream>
 
-constexpr int N = 16;
-constexpr int SIZE = N * N;
-constexpr int RAND_LOW = 0;
-constexpr int RAND_HIGH = 1;
-
 #define checkCudaErrors(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
 {
@@ -27,10 +22,15 @@ void randomizeMatrix(int* matrix, const int size);
 void printMatrix(int* matrix, const int size);
 
 // Kernel
-__global__ void multiplyKernel(int* A, int* B, int* C, const int size);
+__global__ void multiplyKernel(const int* A, const int* B, int* C, const int size);
 
 int main()
 {
+	constexpr int N = 16;
+	constexpr int BYTES = N * N * sizeof(int);
+	constexpr int RAND_LOW = 0;
+	constexpr int RAND_HIGH = 1;
+
 	srand(time(NULL));
 
 	int* A_host = nullptr;
@@ -38,15 +38,17 @@ int main()
 	int* C_host = nullptr;
 
 	// Allocate memory on host for A, B, C matrices
-	checkCudaErrors(cudaMallocHost(&A_host, SIZE));
-	checkCudaErrors(cudaMallocHost(&B_host, SIZE));
-	checkCudaErrors(cudaMallocHost(&C_host, SIZE));
+	checkCudaErrors(cudaMallocHost(&A_host, BYTES));
+	checkCudaErrors(cudaMallocHost(&B_host, BYTES));
+	checkCudaErrors(cudaMallocHost(&C_host, BYTES));
+
+	cudaDeviceSynchronize();
 
 	randomizeMatrix(A_host, N);
 	randomizeMatrix(B_host, N);
 
-	// Zero C-result array
-	memset(C_host, 0, SIZE);
+	// Zero C-result array XDD
+	// memset(C_host, 0, SIZE);
 
 	//printMatrix(A_host, N);
 	//printMatrix(B_host, N);
@@ -59,19 +61,22 @@ int main()
 	// Zero copy
 
 	// Allocate memory on device
-	checkCudaErrors(cudaMalloc(&A_device, SIZE));
-	checkCudaErrors(cudaMalloc(&B_device, SIZE));
-	checkCudaErrors(cudaMalloc(&C_device, SIZE));
+	checkCudaErrors(cudaMalloc(&A_device, BYTES));
+	checkCudaErrors(cudaMalloc(&B_device, BYTES));
+	checkCudaErrors(cudaMalloc(&C_device, BYTES));
 
 	// Sync device memory to host memory
 	checkCudaErrors(cudaHostGetDevicePointer(&A_device, A_host, 0));
 	checkCudaErrors(cudaHostGetDevicePointer(&B_device, B_host, 0));
 	checkCudaErrors(cudaHostGetDevicePointer(&C_device, C_host, 0));
 
-	dim3 threadsPerBlock(N, N);
-	dim3 blocksPerGrid(1, 1);
+	int BLOCK_SIZE = 16;	
+	int GRID_SIZE = (int)ceil(N / BLOCK_SIZE);
 
-	multiplyKernel <<<blocksPerGrid, threadsPerBlock >>> (A_device, B_device, C_device, N);
+	dim3 grid(GRID_SIZE, GRID_SIZE);
+	dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+
+	multiplyKernel <<<grid, threads >>> (A_device, B_device, C_device, N);
 
 	cudaDeviceSynchronize();
 	printMatrix(C_host, N);
@@ -79,26 +84,18 @@ int main()
 	return 0;
 }
 
-__global__ void multiplyKernel(int* A, int* B, int* C, const int size)
+__global__ void multiplyKernel(const int* A, const int* B, int* C, const int size)
 {
-	int col = blockIdx.x * blockDim.x + threadIdx.x;
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
-	int sum = 0;
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-	__shared__ float Msub[16 * 16];
-
-	if (row < size && col < size)
+	C[row * size + col] = 0;
+	for (int i = 0; i < size; ++i)
 	{
-		for (int i = 0; i < size; ++i)
-		{
-			printf("A: row=%d,col=%d,i=%d, value=%d\n", row, col, i, A[row * size + i]);
-			printf("B: row=%d,col=%d,i=%d, value=%d\n", row, col, i, B[row * size + i]);
-			sum += A[row * size + i] * B[i * size + col];
-		}
+		// printf("A: row=%d,col=%d,i=%d, value=%d\n", row, col, i, A[row * size + i]);
+		// printf("B: row=%d,col=%d,i=%d, value=%d\n", row, col, i, B[row * size + i]);
+		C[row * size + col] += A[row * size + i] * B[i * size + col];
 	}
-	
-	C[row * size + col] = sum;
-	// printf("C: row=%d,col=%d, value=%d\n", row, col, sum);
 }
 
 void randomizeMatrix(int* matrix, const int size)
@@ -128,4 +125,23 @@ void printMatrix(int* matrix, const int size)
 int getRandomValue(const int low, const int high)
 {
 	return low + static_cast<int>(rand()) * (static_cast<int>(high - low) / RAND_MAX);
+}
+
+void verify(int* A, int* B, int* C, const int N)
+{
+	// For every row...
+	for (int i = 0; i < N; i++) {
+		// For every column...
+		for (int j = 0; j < N; j++) {
+			// For every element in the row-column pair
+			int tmp = 0;
+			for (int k = 0; k < N; k++) {
+				// Accumulate the partial results
+				tmp += A[i * N + k] * B[k * N + j];
+			}
+
+			// Check against the CPU result
+			assert(tmp == c[i * N + j]);
+		}
+	}
 }
