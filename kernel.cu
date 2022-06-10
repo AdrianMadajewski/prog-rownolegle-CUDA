e49c26f8bf17c0b2c94422e3ec21a087f48c9dee
Adrian Madajewski <madajewski.adrian@gmail.com>
2022-06-10 18:51:58 +0200

    finished???

--- kernel.cu
+++ kernel.cu
@@ -13,12 +13,13 @@
 // https://stackoverflow.com/questions/35535831/is-there-any-difference-between-cudamallochost-and-cudahostalloc-without-spe
 // https://github.com/CoffeeBeforeArch/cuda_programming/blob/master/02_matrix_mul/tiled/mmul.cu
 
-constexpr int N = 1 << 10;					// 1024
-constexpr int SHARED_MEMORY_SIZE = 1 << 10; // 1024
+// 1 << 10 = 1024
+constexpr int N = 1 << 10;
+constexpr int SHARED_MEMORY_SIZE = 1 << 10;
 
 constexpr int BYTES = N * N * sizeof(int);
-constexpr int RAND_LOW = 0;
-constexpr int RAND_HIGH = 1;
+constexpr int RAND_LOW = 1;
+constexpr int RAND_HIGH = 10;
 
 #define checkCudaErrors(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
@@ -33,7 +34,7 @@ inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort =
 int getRandomValue(const int low, const int high);
 void randomizeMatrix(int* matrix, const int size);
 void printMatrix(int* matrix, const int size);
-void verify(int* A, int* B, int* C, const int N);
+bool verify(int* A, int* B, int* C, const int N);
 
 // Kernel
 __global__ void multiplyKernel(const int* __restrict__ A, 
@@ -52,6 +53,7 @@ int main(int argc, char **argv)
 	// Allocate memory on host for A, B, C matrices
 	// https://docs.nvidia.com/cuda/cuda-c-best-practices-guide/index.html#shared-memory-and-memory-banks
 
+	// Prepare paged memory
 	checkCudaErrors(cudaSetDeviceFlags(cudaDeviceMapHost));
 	checkCudaErrors(cudaHostAlloc(&A_host, BYTES, cudaHostAllocMapped));
 	checkCudaErrors(cudaHostAlloc(&B_host, BYTES, cudaHostAllocMapped));
@@ -61,6 +63,12 @@ int main(int argc, char **argv)
 
 	randomizeMatrix(A_host, N);
 	randomizeMatrix(B_host, N);
+
+	// printMatrix(A_host, N);
+	// printMatrix(B_host, N);
+
+	// Set C-matrix to 0s
+	checkCudaErrors(cudaMemset(C_host, 0, BYTES));
 
 	int* A_device = nullptr;
 	int* B_device = nullptr;
@@ -78,23 +86,37 @@ int main(int argc, char **argv)
 	checkCudaErrors(cudaHostGetDevicePointer(&B_device, B_host, 0));
 	checkCudaErrors(cudaHostGetDevicePointer(&C_device, C_host, 0));
 
-	int BLOCK_SIZE = 64;	
-	int GRID_SIZE = (int)ceil(N / BLOCK_SIZE);
+	int THREADS = 32; // 32 MAX	-- Warp size: 32
+	int BLOCKS = (int)ceil(N / THREADS);
 
-	dim3 grid(GRID_SIZE, GRID_SIZE);
-	dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+	dim3 threads(THREADS, THREADS);
+	dim3 blocks(BLOCKS, BLOCKS);
 
-	multiplyKernel <<<grid, threads >>> (A_device, B_device, C_device, N);
+	std::cout << "[DEVICE]: Multiply kernel started\n";
+	multiplyKernel <<<blocks, threads >>> (A_device, B_device, C_device, N);
+	checkCudaErrors(cudaDeviceSynchronize());
+	std::cout << "[DEVICE]: Multiply kernel finished\n";
 
-	cudaDeviceSynchronize();
-
-	verify(A_host, B_host, C_host, N);
+	std::cout << "[CPU]: Verify CPU started\n";
+	bool correct = verify(A_host, B_host, C_host, N);
+	std::cout << "[CPU]: Verify CPU finished\n";
 	// printMatrix(C_host, N);
 
-	printf("COMPLETED WITH SUCCESS\n");
+	// Free memory
+	cudaFree(A_host);
+	cudaFree(B_host);
+	cudaFree(C_host);
+	cudaFree(A_device);
+	cudaFree(B_device);
+	cudaFree(C_device);
+
+	std::cout << correct ? "COMPLETED SUCCESFULLY\n" : "COMPLETED WITH ERROR (verify)\n";
+	
 	return 0;
 }
 
+// https://stackoverflow.com/questions/43235899/cuda-restrict-tag-usage
+// 
 __global__ void multiplyKernel(const int* __restrict__ A, 
 	const int* __restrict__ B, 
 	int* __restrict__ C, 
@@ -138,7 +160,7 @@ void randomizeMatrix(int* matrix, const int size)
 	{
 		for (int j = 0; j < size; ++j)
 		{
-			matrix[i * size + j] = 1;
+			matrix[i * size + j] = getRandomValue(RAND_LOW, RAND_HIGH);
 		}
 	}
 }
@@ -158,25 +180,22 @@ void printMatrix(int* matrix, const int size)
 
 int getRandomValue(const int low, const int high)
 {
-	return low + static_cast<int>(rand()) * 
-		(static_cast<int>(high - low) / RAND_MAX);
+	return rand() % high + low;
 }
 
-void verify(int* A, int* B, int* C, const int N)
+bool verify(int* A, int* B, int* C, const int N)
 {
-	// For every row...
-	for (int i = 0; i < N; i++) {
-		// For every column...
-		for (int j = 0; j < N; j++) {
-			// For every element in the row-column pair
+	for (int row = 0; row < N; ++row) {
+		for (int col = 0; col < N; ++col) {
 			int tmp = 0;
-			for (int k = 0; k < N; k++) {
-				// Accumulate the partial results
-				tmp += A[i * N + k] * B[k * N + j];
+			for (int element = 0; element < N; ++element) {
+				tmp += A[row * N + element] * B[element * N + col];
 			}
 
 			// Check against the CPU result
-			assert(tmp == C[i * N + j]);
+			if (tmp != C[row * N + col])
+				return false;
 		}
 	}
+	return true;
 }
